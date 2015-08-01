ea62e222ab97ec5c8e476f360138a874080cbdba
Max Kayen <mkayen@gmail.com>
2015-07-31 23:29:36 -0400

    testing out heroku

--- seeds/seed.js
+++ seeds/seed.js
@@ -11,3 +11,33 @@ node algo.js > cache/optLineup.html
 // var masterFunction = require(../seeds/numberfire)
 
 // masterFunction()
+
+var mongoose = require('mongoose');
+var Q = require('q');
+var path = require('path');
+
+// Crawlers
+
+var pitcherCrawl = require('../crawlers/numberfire_pitchers.js')
+var hitterCrawl = require('../crawlers/numberfire.js')
+
+// Models
+
+var numberfire_hitters = mongoose.model('Player');
+var numberfire_pitchers = require('nfPitcher');
+
+// Seed Files
+
+var numberfire_hitters_seed = require('numberfire')
+var numberfire_pitchers_seed = require('numberfire_pitchers')
+
+// Define DB Connection
+
+var DATABASE_URI = "mongodb://localhost:27017/dailyfs"
+var db = mongoose.connect(DATABASE_URI).connection;
+
+// Seed
+
+var seed = function(){
+	pitcherCrawl().then(hitterCrawl());
+}
